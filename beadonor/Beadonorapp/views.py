5f6ed1dda0f20d1bf5901abf1b235dda591466ee
Im-your-hari <baluoger91@gmail.com>
2022-02-16 21:03:42 +0530

    --- Auto-Git Commit ---

--- beadonor/Beadonorapp/views.py
+++ beadonor/Beadonorapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse('<h1>First Landing page</h1>')
+def index(request,*args,**kwargs):
+    return render(request, 'index.html',{})
