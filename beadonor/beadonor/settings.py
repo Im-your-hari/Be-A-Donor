9101ba2763d24870cf4037bdcb14887a705d13fa
Im-your-hari <baluoger91@gmail.com>
2022-02-17 14:31:11 +0530

    App folder deleted because of some unwanted errors.

--- beadonor/beadonor/settings.py
+++ beadonor/beadonor/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_FILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_FILES_DIRS = [os.path.join(BASE_DIR,'static/')]
