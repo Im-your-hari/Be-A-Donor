7255d334a94167ba08339dc5e46376f54eca1975
Im-your-hari <baluoger91@gmail.com>
2022-03-04 13:53:15 +0530

    --- Auto-Git Commit ---

--- app/models.py
+++ app/models.py
@@ -2,4 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Search(models.Model):
-    
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    district = models.CharField(max_length=50)
+    blood = models.CharField(max_length=10)
+    phone = models.CharField(max_length=15)
+
+class Join(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    district = models.CharField(max_length=50)
+    blood = models.CharField(max_length=10)
+    phone = models.CharField(max_length=15)
