da06f3f6b9d80b6dee1db87dbefe6a2102d583b5
Im-your-hari <baluoger91@gmail.com>
2022-03-04 07:16:54 +0530

    --- Auto-Git Commit ---

---
+++ app/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+#from .models import Modelclassname
+'''
+class Formclass(forms.ModelForm):
+    class Meta:
+        model = Modelclassname
+        fields = "__all__"
+'''
--- app/views.py
+++ app/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+#from .forms import Formclass
 
 # Create your views here.
 def index(request,*args,**kwargs):
     return render(request, 'index.html',{})
+'''
+def form_create_view(request, *args,**kwargs):
+    form=Formclass(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form=Formclass()
+    context={
+        'form': form,
+    }
+    return render(request, 'index.html', context)
+'''
