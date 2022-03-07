9459485a5c0b087dd59bb31e207dbbd855b3ef8b
Im-your-hari <baluoger91@gmail.com>
2022-03-07 21:50:42 +0530

    --- Auto-Git Commit ---

--- app/views.py
+++ app/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from .models import Search,Join
 #from .forms import Formclass
-'''
+
 searchobj = Search.objects.all()
 search_context = {
     "searchobj" : searchobj
 }
-'''
+
 # Create your views here.
 def index(request,*args,**kwargs):
     return render(request, 'index.html',{})
 
 def search(request,*args,**kwargs):
-    return render(request, 'search.html',{})
+    return render(request, 'search.html',search_context)
 '''
 def form_create_view(request, *args,**kwargs):
     form=Formclass(request.POST or None)
