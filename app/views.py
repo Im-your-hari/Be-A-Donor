e572009aad22f2b015cbaaf1ce5ff934599fdb07
Im-your-hari <baluoger91@gmail.com>
2022-03-07 21:16:25 +0530

    Search page added with sample table..

--- app/views.py
+++ app/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from .models import Search,Join
 #from .forms import Formclass
-
+'''
+searchobj = Search.objects.all()
+search_context = {
+    "searchobj" : searchobj
+}
+'''
 # Create your views here.
 def index(request,*args,**kwargs):
     return render(request, 'index.html',{})
+
+def search(request,*args,**kwargs):
+    return render(request, 'search.html',{})
 '''
 def form_create_view(request, *args,**kwargs):
     form=Formclass(request.POST or None)
@@ -17,7 +25,3 @@ def form_create_view(request, *args,**kwargs):
     return render(request, 'index.html', context)
 '''
 
-searchobj = Search.objects.all()
-search_context = {
-    "searchobj" = searchobj,
-}
